e0c0c68bee76bdeb89ba52eca256a8e0b6d88a50
Kartik <kartikchhoker108@gmail.com>
2026-01-18 21:07:34 +0530

    created a encrytion for token

--- internal/crypto/vault_test.go
+++ internal/crypto/vault_test.go
@@ -9,23 +9,23 @@ func TestEncryption(t *testing.T) {
 	secret := "MySuperSecretToken_123"
 	fmt.Println("Testing with secret:", secret)
 
-	// 1. Try to Encrypt
+	// Try to Encrypt
 	encrypted, err := Encrypt(secret)
 	if err != nil {
 		t.Fatalf("Encryption failed: %v", err)
 	}
 	fmt.Println("Encrypted Hex:", encrypted)
 
-	// 2. Try to Decrypt
+	// Try to Decrypt
 	decrypted, err := Decrypt(encrypted)
 	if err != nil {
 		t.Fatalf("Decryption failed: %v", err)
 	}
 
-	// 3. Verify they match
+	//Verify they match
 	if decrypted != secret {
 		t.Errorf("Expected %s but got %s", secret, decrypted)
 	} else {
-		fmt.Println("✅ Success: Decrypted value matches original!")
+		fmt.Println("Success: Decrypted value matches original!")
 	}
 }
